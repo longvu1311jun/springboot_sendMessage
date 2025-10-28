a015c30c997bed28e30e7607026be731f7913240
Jun <icloudcualong@gmail.com>
2025-10-28 16:56:55 +0700

    update get ID

--- src/main/java/org/example/demoweb/LarkSendMessageController.java
+++ src/main/java/org/example/demoweb/LarkSendMessageController.java
@@ -2,6 +2,8 @@ package org.example.demoweb;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,4 +33,11 @@ public class LarkSendMessageController {
         }
         return "index";
     }
+  @GetMapping("/id/{id}")
+  public String showMessage(@PathVariable("id") String id, Model model) {
+    System.out.println("Nhận được ID: " + id);
+    model.addAttribute("id", id);
+
+    return "getID";
+  }
 }
